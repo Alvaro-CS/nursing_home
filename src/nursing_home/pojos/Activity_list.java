9dc76d53016cb7fd833b47e27c47b18f3c1eea4f
susana <susana@asus>
2019-05-15 21:11:32 +0200

    SUSANA
    activity_list pojo
    started marshall and unmarshall for activities
    getdosagefromtreatment

---
+++ src/nursing_home/pojos/Activity_list.java
@@ -0,0 +1,42 @@
+package nursing_home.pojos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "Activity_list")
+@XmlType(propOrder = {"activities" })
+
+public class Activity_list {
+	@XmlElement
+	private List<Activity> activities;
+
+
+	@Override
+	public String toString() {
+		return "Activity_list [activities=" + activities + "]";
+	}
+
+	public List<Activity> getActivities() {
+		return activities;
+	}
+
+	public Activity_list(List<Activity> activities) {
+		super();
+		this.activities = activities;
+	}
+
+	public void setActivities(List<Activity> activities) {
+		this.activities = activities;
+	}
+
+	public Activity_list() {
+		super();
+	}
+	
+}
